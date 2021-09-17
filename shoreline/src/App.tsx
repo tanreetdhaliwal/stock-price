054d4fb4fb09bd34c9cdd039180e952c62f7e5bc
Tanreet Dhaliwal <tanreet.dhaliwal@gmail.com>
2021-09-16 23:40:43 -0400

    added pages folder

--- shoreline/src/App.tsx
+++ shoreline/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header name="tanreet" />
+      <Home />
     </div>
   );
 }
---
+++ shoreline/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home: React.FunctionComponent = (props) => {
+  return (
+    <div>
+      <Header name="tanreet" />
+    </div>
+  );
+};
+
+export default Home;
